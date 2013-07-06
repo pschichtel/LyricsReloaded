b67e0bc442d4a2d511e5d727dec91d90c7a1624e
Phillip Schichtel <quick_wango@code-infection.de>
2013-07-06 21:56:51 +0200

    fixed metrolyrics (again) and tested oldieslyrics

---
+++ LyricsUnitTests/OldiesLyricsTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CubeIsland.LyricsReloaded.Provider;
+
+namespace LyricsUnitTests
+{
+    [TestClass]
+    public class OldiesLyrics
+    {
+        private static readonly Provider p = LyricsTests.getProvider("Oldies Lyrics");
+
+        [TestMethod]
+        public void oldiesLyricsBasics()
+        {
+            String lyr = p.getLyrics("RODNEY CROWELL", "Why Don't We Talk About It", "");
+
+            Console.WriteLine(lyr);
+
+            Assert.IsFalse(String.IsNullOrWhiteSpace(lyr), "Lyrics not found!");
+        }
+    }
+}
