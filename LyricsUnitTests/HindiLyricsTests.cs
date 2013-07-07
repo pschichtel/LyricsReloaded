bc4c708b881c755fc2b63fdbf1bd25fd18951035
Phillip Schichtel <quick_wango@code-infection.de>
2013-07-07 05:04:12 +0200

    all basic unit tests implemented

---
+++ LyricsUnitTests/HindiLyricsTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CubeIsland.LyricsReloaded.Provider;
+
+namespace LyricsUnitTests
+{
+    [TestClass]
+    public class HindiLyricsTests
+    {
+        private static readonly Provider PROVIDER = LyricsTests.getProvider("Hindi Lyrics");
+
+        [Timeout(3000)]
+        [TestMethod]
+        public void hindiLyricsBasics()
+        {
+            String lyr = PROVIDER.getLyrics("", "Raanjhna Hua Main Tera", "");
+
+            Console.WriteLine(lyr);
+
+            Assert.IsFalse(String.IsNullOrWhiteSpace(lyr), "Lyrics not found!");
+        }
+    }
+}
---
+++ LyricsUnitTests/UrbanLyricsTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CubeIsland.LyricsReloaded.Provider;
+
+namespace LyricsUnitTests
+{
+    [TestClass]
+    public class UrbanLyricsTests
+    {
+        private static readonly Provider PROVIDER = LyricsTests.getProvider("Urban Lyrics");
+
+        [Timeout(3000)]
+        [TestMethod]
+        public void urbanLyricsBasics()
+        {
+            String lyr = PROVIDER.getLyrics("50 Cent", "Buzzin' (Remix)", "");
+
+            Console.WriteLine(lyr);
+
+            Assert.IsFalse(String.IsNullOrWhiteSpace(lyr), "Lyrics not found!");
+        }
+    }
+}
